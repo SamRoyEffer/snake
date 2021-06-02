0bf04c2c7737d55da133843db85b11c93412ccb5
Sam Roy-Effer <samroyeffer@gmail.com>
2021-06-02 12:56:31 -0400

    gave the snake a name

--- play.js
+++ play.js
@@ -1,24 +1,7 @@
-const net = require("net");
 
+const connect = require("./client")
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "135.23.222.131",
-    port: 50542,
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  
-  
-  conn.on("connect", () => {
-    conn.setEncoding("utf8");
-    console.log("incomming data")
-  });
-
-
-  return conn;
-};
 
 
 
